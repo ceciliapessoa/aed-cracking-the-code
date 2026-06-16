d21e0072cf21208394101cc8b04800f75dcc6468
Cecilia Pessoa <113480240+Cecihp@users.noreply.github.com>
2026-06-16 18:54:04 -0300

    Resolução da terceira questão

--- challenges/lowest_common_ancestor.py
+++ challenges/lowest_common_ancestor.py
@@ -6,4 +6,14 @@ def lowest_common_ancestor(
     value1: int,
     value2: int,
 ) -> int:
-    raise NotImplementedError
+     atual = root
+     while atual:
+
+        if value1 < atual.value and value2 < atual.value:
+            atual = atual.left
+
+        elif value1 > atual.value and value2 > atual.value:
+            atual = atual.right
+
+        else:
+            return atual.value
