e3476f6288dd242266f82929463fe0aa58c4420f
Cecilia Pessoa <113480240+Cecihp@users.noreply.github.com>
2026-06-16 18:53:24 -0300

    Resolução da quarta questão

--- challenges/tree_height.py
+++ challenges/tree_height.py
@@ -2,4 +2,11 @@ from data_structures.node import Node
 
 
 def tree_height(root: Node | None) -> int:
-    raise NotImplementedError
+    atual = root
+    if root is None:
+        return -1
+
+    esquerda = tree_height(root.left)
+    direita = tree_height(root.right)
+
+    return 1 + max(esquerda, direita)
