36cac9a275fde1618084ba9b356c69b88d9119d3
Cecilia Pessoa <113480240+Cecihp@users.noreply.github.com>
2026-06-16 18:50:21 -0300

    Resolução da segunda questão

--- challenges/has_cycle.py
+++ challenges/has_cycle.py
@@ -1,2 +1,27 @@
 def has_cycle(graph: dict[str, list[str]]) -> bool:
-    raise NotImplementedError
+    visitados = set()
+    caminho = set()
+
+    def dfs(no):
+        if no in caminho:
+            return True
+
+        if no in visitados:
+            return False
+
+        visitados.add(no)
+        caminho.add(no)
+
+        for vizinho in graph.get(no, []):
+            if dfs(vizinho):
+                return True
+
+        caminho.remove(no)
+
+        return False
+
+    for no in graph:
+        if dfs(no):
+            return True
+
+    return False
